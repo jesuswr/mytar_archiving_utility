c5f071debe936bbbb5c9bf9768dcefd279498eb7
neilvillamizar <neilvillamizar@gmail.com>
2019-10-28 23:05:26 -0400

    testing unpacker

--- unpacker.c
+++ unpacker.c
@@ -9,10 +9,98 @@
 #include "tar.h"
 #include "utilities.h"
 
+int leer_aux( int fd , char* buf , int l )
+{
+	int l2;
+	l2 = 0;
+	while ( l2 < l )
+	{
+		l2 = l2 + read( fd , buf + l2 , l - l2 );
+	}
+	return 0;
+}
 
-unpack( char * packed_file){
+int str_to_int( char* c  )
+{
+	int x, i;
+	x = 0;
+	for( i = 3 ; i >= 0 ; i-- )
+	{
+		x = x << 8;
+		x = x | c[i];
+	}
+	return x;
+}
+
+void write_aux(int fd, int len, char * buf){
+	int len2 = 0;
+	while(len2 < len){
+		len2 += write(fd, buf + len2, len-len2);
+	}
+}
+
+
+int loaddata(int fd1 , int fd2 , int size){
+
+	printf("qlqmano in\n");
 	
-	int fd;
+	int read_count = 0;
+	char * buff = (char*) malloc(sizeof(char)*size);
+
+	leer_aux(fd1, buff, size);
+	write_aux(fd2, size, buff);
+	
+	free(buff);
+
+	printf("qlqmano out\n");
+}
+
+int read_header( int fd , header* h ){
+	/* RECORDAR LEER LA CASILLA LIBRE ANTES DE LLAMAR LA FUNCION */
+	char buf[4];
+	char *buf2, *buf3;
+	int e;
+
+	printf("jeje\n");
+
+	e = leer_aux( fd , buf , 4 );
+	h->modo = str_to_int( buf );
+
+	e = leer_aux( fd , buf , 4 );
+	h->uid = str_to_int( buf );
+
+	e = leer_aux( fd , buf , 4 );
+	h->gid = str_to_int( buf );
+	
+	e = leer_aux( fd , buf , 4 );
+	h->size = str_to_int( buf );
+
+	e = leer_aux( fd , buf , 4 );
+	h->num_blocks = str_to_int( buf );
+
+	e = leer_aux( fd , buf , 4 );
+	h->name_size = str_to_int( buf );
+
+	e = leer_aux( fd , buf , 4 );
+	h->link_size = str_to_int( buf );
+
+	buf2 = (char*)malloc( h->size );
+	e = leer_aux( fd , buf2 , h->name_size );
+	h->name = buf2;
+	if( h->link_size > 0 ){
+		buf3 = (char*)malloc( h->link_size );
+		e = leer_aux( fd , buf3 , h->link_size);
+		h->link_path = buf3;
+	}
+
+	printf("jojo\n");
+
+	return 0;
+}
+
+int unpack( char * packed_file){
+	
+	int fd, fd2;
 	header h;
 	char buf;
 
@@ -20,7 +108,11 @@ unpack( char * packed_file){
 
 	while ( read(fd , &buf, 1)==1 && (buf=='D' || buf=='C') ){
 
+		printf("aiuda\n");
+
 		read_header(fd, &h);
+
+		printf("ooooo\n");
 
 		if ( (h.modo & __S_IFDIR) == __S_IFDIR ){
 			mkdir(h.name, h.modo);
@@ -44,5 +136,5 @@ unpack( char * packed_file){
 
 
 int main(){
-	//
+	unpack("mytar");
 }
