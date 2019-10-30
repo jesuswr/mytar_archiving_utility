4927823f01d8e50597fdc4eb949250d9f3b13106
jesuswr <jesuswahrman@gmail.com>
2019-10-30 08:14:23 -0400

    comments and fixes

--- utilities.c
+++ utilities.c
@@ -1,8 +1,26 @@
+/*
+*	File:			utilities.h
+*	Description:	file that contains the implementation of some useful
+*					functions used in tar
+*	Date:			29 / 10 / 19
+*/
+
 #include "tar.h"
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 
+/*
+*	Function : int_to_char
+*	-----------------------------------------------------------
+*		stores the first byte of the int to the first position of the
+*		array, then right shifts the int bits by 8 and repeats the
+*		process now with the second position of the array, then for
+*		the third and finally for the fourth
+*
+*		x : int to store
+*		ret : pointer to the array of unsigned chars
+*/
 
 void int_to_char(int x, unsigned char * ret){
 	int i;
@@ -12,38 +30,93 @@ void int_to_char(int x, unsigned char * ret){
 	}
 }
 
-void write_aux(int fd, int len, unsigned char * buf){
-	int len2 = 0;
+/*
+*	Function : write_aux
+*	-----------------------------------------------------------
+*		makes calls to syscall read until all len is read or an 
+*		error occurs
+*
+*		fd : file descriptor of the file to write
+*		len : ammount of chars to write from buf
+*		buf : array of unsigned chars to write from
+*
+*		returns 0 in case of success or -1 in case of failure		
+*/
+int write_aux(int fd, int len, unsigned char * buf){
+	int e , len2 = 0;
 	while(len2 < len){
-		len2 += write(fd, buf + len2, len-len2);
+		e = write(fd, buf + len2, len-len2);
+		if ( e <= 0 ) return -1;
+		len2 += e;
 	}
+	return 0;
 }
-
-unsigned char rotar_char(unsigned char a, int desp){
+/*
+*	Function : rotate_char
+*	-----------------------------------------------------------
+*		uses bit magic to rotate the bits easily to the left
+*
+*		a : unsigned char to rotate the bits from
+*		desp : number of times to rotate
+*
+*		returns an unsigned char with the rotated bits
+*/
+unsigned char rotate_char(unsigned char a, int desp){
 	int i;
 	a = ( a << desp ) | ( a >> 8 - desp );
 	return a;
 }
 
-void cifrar_string(unsigned char * s, int len, int desp){
+/*
+*	Function : encode_string
+*	-----------------------------------------------------------
+*		makes calls to rotate_char for all the chars in the string,
+*		uses 8 - ( desp % 8 ) because its the same rotate to the left
+*		desp times than rotate to the right 8 - ( desp % 8 )
+*
+*		s : array of chars to rotate
+*		len : size of the array
+*		desp : number of times to rotate	
+*/
+void encode_string(unsigned char * s, int len, int desp){
 	int i;
-	for(i=0; i<len; i++) s[i] = rotar_char(s[i], desp%8);
+	for(i=0; i<len; i++) s[i] = rotate_char(s[i], 8 -(desp%8));
 }
 
-void descifrar_string(unsigned char * s, int len, int desp){
+/*
+*	Function : decode_string
+*	-----------------------------------------------------------
+*		makes calls to rotate_char for all the chars in the string
+*
+*		s : array of chars to rotate
+*		len : size of the array
+*		desp : number of times to rotate	
+*/
+
+void decode_string(unsigned char * s, int len, int desp){
 	int i;
-	for(i=0; i<len; i++) s[i] = rotar_char(s[i], 8-(desp%8));
+	for(i=0; i<len; i++) s[i] = rotate_char(s[i], desp%8);
 }
 
-unsigned char* make_path( unsigned char* path , unsigned char* name )
-{
-	unsigned char *ret = (unsigned char *)malloc( strlen(path) + strlen(name) + 1);
-	if ( strlen(path) == 0 )
-	{
+
+/*
+*	Function : make_path
+*	-----------------------------------------------------------
+*		given two arrays of chars, uses string.h basic functions to
+*		create a new string with the format "path/name"
+*
+*		path : pointer to the path name
+*		name : pointer to the file name
+
+*		returns an array of chars of the form path/name	
+*/
+unsigned char* make_path( unsigned char* path , unsigned char* name ){
+	unsigned char *ret = 
+		(unsigned char *)malloc( strlen(path) + strlen(name) + 1);
+	if ( strlen(path) == 0 ){
 		strcpy( ret , name );	
 	}
-	else
-	{
+	else{
 		strcpy( ret , path );
 		ret[strlen(path)] = '/';
 		strcat( ret, name);
@@ -51,23 +124,45 @@ unsigned char* make_path( unsigned char* path , unsigned char* name )
 	return ret;
 }
 
-int leer_aux( int fd , unsigned char* buf , int l )
-{
-	int l2;
+/*
+*	Function : read_aux
+*	-----------------------------------------------------------
+*		makes calls to syscall read untile l chars are read and 
+*		stored in buf or an error occurs
+*
+*		fd : file descriptor of the file to read from
+*		len : ammount of chars to read 
+*		buf : array of unsigned chars to write 
+*
+*		returns 0 in case of success or -1 in case of failure		
+*/
+int read_aux( int fd , unsigned char* buf , int l ){
+	int l2, e;
 	l2 = 0;
-	while ( l2 < l )
-	{
-		l2 = l2 + read( fd , buf + l2 , l - l2 );
+	while ( l2 < l ){
+		e = read( fd , buf + l2 , l - l2 );
+		if ( e <= 0 ) return -1;
+		l2 = l2 + e;
 	}
 	return 0;
 }
 
-int str_to_int( unsigned char* c  )
-{
+/*
+*	Function : str_to_int
+*	-----------------------------------------------------------
+*		saves the bits from each character in the int, to do so
+*		it saves the bits from the fourth char, then left shifts 8 bits
+*		and saves the bits from the third, then left shifts 8 bits and
+*		repeats for the second and first char
+*
+*		c : pointer to the array of chars
+*
+*		returns an int that has the bits of the array
+*/
+int str_to_int( unsigned char* c  ){
 	int x, i;
 	x = 0;
-	for( i = 3 ; i >= 0 ; i-- )
-	{
+	for( i = 3 ; i >= 0 ; i-- ){
 		x = x << 8;
 		x = x | c[i];
 	}
--- utilities.h
+++ utilities.h
@@ -1,20 +1,107 @@
+/*
+*	File:			utilities.h
+*	Description:	file that contains the signature of some useful
+*					functions used in tar
+*	Date:			29 / 10 / 19
+*/
+
 #ifndef _UTILITIES_H
 #define _UTILITIES_H
 
+/*
+*	Function : int_to_char
+*	-----------------------------------------------------------
+*		stores the bytes of the int in an char array of size 4
+*
+*		x : int to store
+*		ret : pointer to the array of unsigned chars
+*/
 void int_to_char(int x, unsigned char * ret);
 
-void write_aux(int fd, int len, unsigned char * buf);
+/*
+*	Function : write_aux
+*	-----------------------------------------------------------
+*		helper function to write the content of an array of chars
+*		to the file of the given file descriptor
+*
+*		fd : file descriptor of the file to write
+*		len : ammount of chars to write from buf
+*		buf : array of unsigned chars to write from
+*
+*		returns 0 in case of success or -1 in case of failure		
+*/
+int write_aux(int fd, int len, unsigned char * buf);
 
-unsigned char rotar_char(unsigned char a, int desp);
+/*
+*	Function : rotate_char
+*	-----------------------------------------------------------
+*		rotates the bits of a char to the right the given number of times
+*
+*		a : unsigned char to rotate the bits from
+*		desp : number of times to rotate
+*
+*		returns an unsigned char with the rotated bits
+*/
+unsigned char rotate_char(unsigned char a, int desp);
 
-void cifrar_string(unsigned char * s, int len, int desp);
+/*
+*	Function : encode_string
+*	-----------------------------------------------------------
+*		rotates the bits of all the chars in the given array to the right
+*
+*		s : array of chars to rotate
+*		len : size of the array
+*		desp : number of times to rotate	
+*/
+void encode_string(unsigned char * s, int len, int desp);
 
-void descifrar_string(unsigned char * s, int len, int desp);
+/*
+*	Function : decode_string
+*	-----------------------------------------------------------
+*		rotates the bits of all the chars in the given array to the left
+*
+*		s : array of chars to rotate
+*		len : size of the array
+*		desp : number of times to rotate	
+*/
+void decode_string(unsigned char * s, int len, int desp);
 
+
+/*
+*	Function : make_path
+*	-----------------------------------------------------------
+*		given two arrays of chars, creates a path of the form "path/name"
+*
+*		path : pointer to the path name
+*		name : pointer to the file name
+
+*		returns an array of chars of the form path/name	
+*/
 unsigned char* make_path( unsigned char* path , unsigned char* name );
 
-int leer_aux( int fd , unsigned char* buf , int l );
+/*
+*	Function : read_aux
+*	-----------------------------------------------------------
+*		helper function to read the content of a file to an
+*		array of unsigned chars
+*
+*		fd : file descriptor of the file to read from
+*		len : ammount of chars to read 
+*		buf : array of unsigned chars to write 
+*
+*		returns 0 in case of success or -1 in case of failure		
+*/
+int read_aux( int fd , unsigned char* buf , int l );
 
+/*
+*	Function : str_to_int
+*	-----------------------------------------------------------
+*		stores the bits of the chars in the array to an int
+*
+*		c : pointer to the array of chars
+*
+*		returns an int that has the bits of the array
+*/
 int str_to_int( unsigned char* c  );
 
 #endif
