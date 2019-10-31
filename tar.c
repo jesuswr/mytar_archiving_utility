7d2224de1f4d583f7f1b1be4887c3225b10dc961
jesuswr <jesuswahrman@gmail.com>
2019-10-30 23:31:55 -0400

    comments

--- tar.c
+++ tar.c
@@ -1,3 +1,11 @@
+/*
+*	File:			tar.c
+*	Author: 		Jesus Wahrman 15-11540 , Neil Villamizar 15-11523
+*	Description:	file that contains the implementation of the mytar 
+*					functions
+*	Date:			29 / 10 / 19
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,6 +17,19 @@
 #include "tar.h"
 #include "utilities.h" 
 
+/*
+*	Function : loaddata
+*	-----------------------------------------------------------
+*		reads size bytes from fd1 and writes them in fd2, using
+*		helper functions, if c > 0 it decodes de data using another helper
+*
+*		fd1 : file descriptor of file to read from
+*		fd2 : file descriptor of file to write from
+*		size : ammount of bytes to copy
+*		c : amount of bits to rotate
+*
+*		returns 0 in case of success or -1 in failure
+*/
 int loaddata(int fd1 , int fd2 , int size, int c){
 	char *buff;
 	int e;
@@ -21,6 +42,17 @@ int loaddata(int fd1 , int fd2 , int size, int c){
 	return 0;
 }
 
+/*
+*	Function : get_header
+*	-----------------------------------------------------------
+*		function that gets the stat of the given file, and
+*		extracts all the information needed to the header structure
+*
+*		path_and_name : path with the name of the file
+*		h : pointer to a header, where the information will be stored
+*
+*		returns 0 in case of success or < 0 in failure
+*/
 int get_header( char* path_and_name , header *h ){
 	
 	struct stat sb;
@@ -72,6 +104,19 @@ int get_header( char* path_and_name , header *h ){
 	return 0;
 }
 
+/*
+*	Function : read_header
+*	-----------------------------------------------------------
+*		function that reads from a tar file, the information that has to
+*		be stored in the header using helper functions. If c > 0 it decodes
+*		the string 
+*
+*		fd : file descriptor of the file to read the data from
+*		h : pointer to a header, where the information will be stored
+*		c : amount of bits to rotate
+*
+*		returns 0 in case of success or < 0 in failure
+*/
 int read_header( int fd , header* h, int c){
 	char buf[4];
 	char *buf2, *buf3;
@@ -133,7 +178,16 @@ int read_header( int fd , header* h, int c){
 	return 0;
 }
 
-
+/*
+*	Function : header_to_string
+*	-----------------------------------------------------------
+*		function that stores the files of a header in a char array,
+*		using helper functions
+*
+*		h : pointer to a header, that contains the information to be saved
+*
+*		returns a pointer to the char array in success of NULL in failure
+*/
 char * header_to_string(header * h){
 	
 	char * ret = NULL;
@@ -156,11 +210,24 @@ char * header_to_string(header * h){
 	int_to_char(h->name_size, (aux + 20));
 	int_to_char(h->link_size, (aux + 24));
 	sprintf((aux + 28), "%s", (char *) h->name);
-	if ( h->link_size > 0 ) sprintf((aux + 28 + h->name_size), "%s", (char *) h->link_path);
+	if ( h->link_size > 0 ) 
+		sprintf((aux + 28 + h->name_size), "%s", (char *) h->link_path);
 
 	return ret;
 }
 
+/*
+*	Function : store_header
+*	-----------------------------------------------------------
+*		function that stores the content of a header in a file, using
+*		the header_to_string function, if c > 0 it encodes the string
+*
+*		h : pointer to a header, that contains the information
+*		fd : file descriptor of the file where the information will be saved
+*		c : amount of bits to rotate
+*
+*		returns 0 in case of success or < 0 in failure
+*/
 int store_header(header * h, int fd, int c){
 	char * buf = header_to_string(h);
 	int e;
@@ -175,8 +242,20 @@ int store_header(header * h, int fd, int c){
 	return 0;
 }
 
+/*
+*	Function : save_data
+*	-----------------------------------------------------------
+*		function that stores the content of a file in another file, 
+*		using helper functions. If c > 0 it encodes the content before
+*		saving it
+*
+*		fd : file descriptor of the file where the data will be saved
+*		path : path of the file to read the data from
+*		c : amount of bits to rotate
+*
+*		returns 0 in case of success or < 0 in failure
+*/
 int save_data( int fd , char* path, int c){
-	
 	int a, l, e;
 	char buff[400];
 
@@ -195,7 +274,24 @@ int save_data( int fd , char* path, int c){
 	return 0;
 }
 
-int pack( int flag_mask, char** argv , int argc, char * pack_file, char * v_output_file, int desp ){ /* flag mask, arreglo de strings a empaquetar, nombre del tar, archivo verboso */
+/*
+*	Function : pack
+*	-----------------------------------------------------------
+*		function that packs the given files in a tar file, with
+*		the especified flags
+*
+*		flag_mask : int that represents what flags are on
+*		argv : array of strings that represent the files to be packed
+*		argc : number of files in argv
+*		pack_file : name of the tar file
+*		v_output_file : name of the file to save the output in,
+*						if there's no name it will go to the terminal
+*		desp : amount of bits to rotate the data bits
+*
+*		returns 0 in case of success or < 0 in failure
+*/
+int pack( int flag_mask, char** argv , int argc, char * pack_file, 
+			char * v_output_file, int desp ){ 
 	
 	int fd;
 	header h;
@@ -208,7 +304,8 @@ int pack( int flag_mask, char** argv , int argc, char * pack_file, char * v_outp
 
 	if((flag_mask & __F_IFV) == __F_IFV){
 		if(v_output_file != NULL) {
-			fd_v_output = open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  S_IRWXU | S_IRWXG | S_IROTH );
+			fd_v_output = open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  
+								S_IRWXU | S_IRWXG | S_IROTH );
 			if ( fd_v_output < 0 ) {
 				printf("Error opening the wanted output file. The output will go to the terminal.\n");
 				fd_v_output = 1;
@@ -222,14 +319,15 @@ int pack( int flag_mask, char** argv , int argc, char * pack_file, char * v_outp
 		if ( e < 0 ) printf("Error trying to write in the wanted file descriptor.\n");
 	}
 
-	fd = open ( pack_file , O_RDWR | O_CREAT | O_TRUNC , S_IRWXU | S_IRWXG | S_IROTH );
+	fd = open ( pack_file , O_RDWR | O_CREAT | O_TRUNC , 
+		S_IRWXU | S_IRWXG | S_IROTH );
 	if ( fd < 0 ){
 		e = write_aux( fd_v_output , 44 , "Error creating the tar with the given name.\n" );
 		if ( e < 0 ) printf("Error creating the tar with the given name.\n");
 		return -1;
 	}
 
-	for ( i = 2 ; i < argc ; i ++){
+	for ( i = 0 ; i < argc ; i ++){
 
 		e = get_header( argv[i] , &h );
 		if ( e < 0 ){
@@ -238,7 +336,8 @@ int pack( int flag_mask, char** argv , int argc, char * pack_file, char * v_outp
 			continue;
 		}
 
-		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == __S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
+		if ((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == __S_IFLNK) || 
+			(( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
 
 		e = store_header( &h , fd, DESP);
 		if ( e < 0 ){
@@ -257,8 +356,10 @@ int pack( int flag_mask, char** argv , int argc, char * pack_file, char * v_outp
 		}
 
 		if ( ( h.modo & __S_IFMT ) == __S_IFLNK) continue;
-		if ( ( h.modo & __S_IFMT ) == __S_IFDIR ) pack_dir(flag_mask, fd , argv[i] );
-		if ( ( h.modo & __S_IFMT ) == __S_IFREG ) save_data( fd , argv[i], DESP);
+		if ( ( h.modo & __S_IFMT ) == __S_IFDIR ) 
+			pack_dir(flag_mask, fd , argv[i] );
+		if ( ( h.modo & __S_IFMT ) == __S_IFREG ) 
+			save_data( fd , argv[i], DESP);
 
 
 		free( h.name );
@@ -267,33 +368,44 @@ int pack( int flag_mask, char** argv , int argc, char * pack_file, char * v_outp
 
 	close( fd );
 	return 0;
-	
 }
 
-void pack_dir(int flag_mask, int fd, char * ruta){
+/*
+*	Function : pack_dir
+*	-----------------------------------------------------------
+*		function that packs the given files in a tar file, with
+*		the especified flags
+*
+*		flag_mask : int that represents what flags are on
+*		fd : file descriptor of the tar file to save the data in
+*		path : path of the current dir that we are saving
+*/
+void pack_dir(int flag_mask, int fd, char * path){
   	DIR* dirp;
   	struct dirent* de;
   	header h;
   	int e;
 
-  	dirp = opendir( ruta );
+  	dirp = opendir( path );
  	if ( dirp == NULL ){
 		e = write_aux( fd_v_output , 27 , "Error opening a directory.\n");
 		if ( e < 0 ) printf("Error opening a directory.\n");
     }
 	while( de = readdir( dirp ) ){
 
-		e = get_header( make_path(ruta, (de->d_name)), &h);
+		e = get_header( make_path(path, (de->d_name)), &h);
 		if ( e < 0 ){
 			e = write_aux( fd_v_output , 40 , "Error using lstat to get the file info.\n");
 			if ( e < 0 ) printf("Error using lstat to get the file info.\n");
 			continue;
 		}
 	
-		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == __S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
+		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == __S_IFLNK) 
+			|| (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
 
 		/* ARREGLAR ESTA LINEA DE ABAJO, LA PARTE DE mytar */
-		if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0 || strcmp(de->d_name, "mytar")==0) continue; /* comparar con el nombre del archivo 'mytar'*/
+		if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0 
+			|| strcmp(de->d_name, "mytar")==0) continue; 
 	
 		e = store_header( &h, fd, DESP);
 		if ( e < 0 ){
@@ -312,9 +424,10 @@ void pack_dir(int flag_mask, int fd, char * ruta){
 		}
 
 		if(( h.modo & __S_IFMT ) == __S_IFLNK) continue;
-		if( (h.modo & __S_IFDIR) == __S_IFDIR) pack_dir(flag_mask, fd, make_path(ruta, (de->d_name)) );
+		if( (h.modo & __S_IFDIR) == __S_IFDIR) 
+			pack_dir(flag_mask, fd, make_path(path, (de->d_name)) );
 		if( (h.modo & __S_IFREG) == __S_IFREG) {
-			e = save_data(fd, make_path(ruta, (de->d_name)), DESP);
+			e = save_data(fd, make_path(path, (de->d_name)), DESP);
 			if ( e < 0 ){
 				e = write_aux( fd_v_output , 23 , "Error saving the data.\n" );
 				if ( e < 0 ) printf("Error saving the data.\n");
@@ -329,8 +442,23 @@ void pack_dir(int flag_mask, int fd, char * ruta){
 
 }
 
-
-int unpack(int flag_mask, char * packed_file, char * v_output_file, char * unpacking_dir, int desp){
+/*
+*	Function : unpack
+*	-----------------------------------------------------------
+*		function that unpacks the given files in a tar file, with
+*		the especified flags
+*
+*		flag_mask : int that represents what flags are on
+*		pack_file : name of the tar file
+*		v_output_file : name of the file to save the output in,
+*						if there's no name it will go to the terminal
+*		unpacking_dir : directory where the files will be created
+*		desp : amount of bits to rotate the data bits
+*
+*		returns 0 in case of success or < 0 in failure
+*/
+int unpack(int flag_mask, char * packed_file, 
+	char * v_output_file, char * unpacking_dir, int desp){
 	
 	int fd, fd2, e;
 	header h;
@@ -344,7 +472,9 @@ int unpack(int flag_mask, char * packed_file, char * v_output_file, char * unpac
 	fd_v_output = 1;
 
 	if((flag_mask & __F_IFV) == __F_IFV){
-		if(v_output_file != NULL) fd_v_output = open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  S_IRWXU | S_IRWXG | S_IROTH );
+		if(v_output_file != NULL) fd_v_output = 
+			open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  
+				S_IRWXU | S_IRWXG | S_IROTH );
 		if ( fd_v_output < 0 ){
 			printf("Error opening the output file. The output will be on the terminal.\n");
 			fd_v_output = 1;
@@ -386,7 +516,8 @@ int unpack(int flag_mask, char * packed_file, char * v_output_file, char * unpac
 			if ( e < 0 ) printf("Error reading the tar file.\n");
 		}
 
-		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == __S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
+		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == 
+			__S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
 
 		if((flag_mask & __F_IFV) == __F_IFV){
 			e = write_aux(fd_v_output, 10, "Unpacking ");
@@ -441,8 +572,22 @@ int unpack(int flag_mask, char * packed_file, char * v_output_file, char * unpac
 	return 0;
 }
 
-/* se llama cuando pasan t, tambien deben pasar f*/
-int show_content_file(int flag_mask, char * packed_file, char * v_output_file){
+/*
+*	Function : show_content_file
+*	-----------------------------------------------------------
+*		shows the files in the tar file, it reads the headers and 
+*		prints the file name and other information like "ls -l"
+*
+*		flag_mask : int that represents what flags are on
+*		pack_file : name of the tar file
+*		v_output_file : name of the file to save the output in,
+*						if there's no name it will go to the terminal
+*		desp : amount of bits to rotate the data bits
+*
+*		returns 0 in case of success or < 0 in failure
+*/
+int show_content_file(int flag_mask, char * packed_file, 
+	char * v_output_file, int desp){
 	
 	int fd, fd2, i, e;
 	header h;
@@ -453,12 +598,16 @@ int show_content_file(int flag_mask, char * packed_file, char * v_output_file){
 	fd_v_output = 1;
 
 	if((flag_mask & __F_IFV) == __F_IFV && (v_output_file != NULL)){
-		fd_v_output = open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  S_IRWXU | S_IRWXG | S_IROTH );
+		fd_v_output = 
+			open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  
+				S_IRWXU | S_IRWXG | S_IROTH );
 		if ( fd_v_output < 0 ){
 			printf("Error opening the output file. The output will be in the terminal.\n" );
 			fd_v_output = 1;
 		}
 	}
+
+	if ( flag_mask & __F_IFY ) DESP = desp;
 	
 	aux =  (char *) malloc(4); 
 	if ( aux == NULL ){
@@ -493,7 +642,8 @@ int show_content_file(int flag_mask, char * packed_file, char * v_output_file){
 			continue;
 		}
 
-		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == __S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
+		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == 
+			__S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
 
 		if( (h.modo & __S_IFMT) == __S_IFDIR ) write_aux(fd_v_output, 1, "d");
 		else if( ( h.modo & __S_IFMT ) == __S_IFLNK ) write_aux(fd_v_output, 1, "l");
@@ -538,6 +688,14 @@ int show_content_file(int flag_mask, char * packed_file, char * v_output_file){
 
 }
 
+/*
+*	Function : write_aux_int
+*	-----------------------------------------------------------
+*		helper function to write ints to the output
+*
+*		fd : file descriptor of the output file
+*		n : number to write
+*/
 void write_aux_int(int fd, int n){
 	char buf[11];
 	int size = 0;
@@ -562,7 +720,24 @@ void write_aux_int(int fd, int n){
 
 }
 
-int single_extract(int flag_mask, char* f_name, char * packed_file, char * v_output_file, char * unpacking_dir, int desp){
+/*
+*	Function : single_extract
+*	-----------------------------------------------------------
+*		function that looks for a file with a specific name in the tar, and
+*		if it finds it, it extracts the file from the tar
+*
+*		flag_mask : int that represents what flags are on
+*		f_name : name of the file to look
+*		pack_file : name of the tar file
+*		v_output_file : name of the file to save the output in,
+*						if there's no name it will go to the terminal
+*		unpacking_dir : directory where the files will be created
+*		desp : amount of bits to rotate the data bits
+*
+*		returns 0 in case of success or < 0 in failure
+*/
+int single_extract(int flag_mask, char* f_name, char * packed_file, 
+	char * v_output_file, char * unpacking_dir, int desp){
 	int fd, fd2, e;
 	header h;
 	char buf, *buf2;
@@ -575,7 +750,9 @@ int single_extract(int flag_mask, char* f_name, char * packed_file, char * v_out
 	fd_v_output = 1;
 
 	if((flag_mask & __F_IFV) == __F_IFV){
-		if(v_output_file != NULL) fd_v_output = open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  S_IRWXU | S_IRWXG | S_IROTH );
+		if(v_output_file != NULL) fd_v_output = 
+			open(v_output_file, O_RDWR | O_CREAT | O_TRUNC,  
+				S_IRWXU | S_IRWXG | S_IROTH );
 		if ( fd_v_output < 0 ){
 			printf("Error opening the output file. The output will be on the terminal.\n");
 			fd_v_output = 1;
@@ -617,7 +794,8 @@ int single_extract(int flag_mask, char* f_name, char * packed_file, char * v_out
 			if ( e < 0 ) printf("Error reading the tar file.\n");
 		}
 
-		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == __S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
+		if((flag_mask & __F_IFN) && ((( h.modo & __S_IFMT ) == 
+			__S_IFLNK) || (( h.modo & __S_IFMT ) == __S_IFIFO))) continue;
 
 		if((flag_mask & __F_IFV) == __F_IFV){
 			e = write_aux(fd_v_output, 10, "Looking ");
